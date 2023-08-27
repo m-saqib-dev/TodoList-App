5c28044d07c34af08569722074c57b0845dc4844
m-saqib-dev <m.saqibsiddiqui.dev@gmail.com>
2023-08-27 16:03:52 +0500

    todo-list component made responsive

--- src/component/TodoList/index.tsx
+++ src/component/TodoList/index.tsx
@@ -1,14 +1,17 @@
+import { useState } from 'react'
 import Button from '../button'
-import { AiOutlineCheckCircle } from 'react-icons/ai'
+import { AiOutlineCheckCircle, AiOutlineMenu, AiTwotoneClockCircle } from 'react-icons/ai'
 
 const TodoList = () => {
+  const [viewMore,setViewMore] = useState("max-sm:overflow-hidden max-sm:h-16");
+
   return (
-    <div className="flex w-4/5 bg-gradient-to-r from-slate-900 to-blue-950 p-4 my-2 rounded-md">
+    <div className="flex w-4/5 bg-gradient-to-r from-slate-900 to-blue-950 p-1 my-2 rounded-md">
     <div className="p-1">
-    <AiOutlineCheckCircle className="text-2xl"/>
+    <AiOutlineCheckCircle className="max-sm:text-lg text-2xl"/>
     </div>
       <div className="flex p-1 text-xs flex-col">
-    <h1 className='w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
+    <h1 onClick={()=>{setViewMore('')}} className={`w-full ${viewMore} bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, unde. Assumenda, suscipit natus quibusdam autem optio earum minus tempora possimus nam iste, quo facere illum laudantium nobis veniam exercitationem animi dicta vero ea! Tenetur consequuntur impedit obcaecati at. At, nesciunt!
       </h1>
       <div className='flex gap-1 py-2'>
@@ -21,14 +24,15 @@ const TodoList = () => {
       </div>
       </div>
           </div>
-    <div className="flex flex-col">
-        <div className="flex">
+    <div className="flex flex-col items-center">
+        <AiOutlineMenu className="m-1 sm:hidden"/>
+        <div className=" max-sm:hidden">
   <Button type='check' onClick={()=>{}} />
   <Button type='trash'  onClick={()=>{}}/>
   <Button type='edit' onClick={()=>{}}/>
         </div>
-        <div className="p-2 text-xs">
-            <div className="end-time">TimeLeft : {}</div>
+        <div className=" py-2 text-xs flex flex-col items-center">
+            <AiTwotoneClockCircle/>
             <h6>9:24</h6>
           </div>
     </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,12 +26,12 @@ const Home = () => {
 
   return (
     <div className='flex justify-center items-center flex-col shrink-0 py-5 gap-y-3'>
-<figure className="max-w-screen-md mx-auto h-64 flex items-center flex-col text-center">
+<figure className="max-w-screen-md mx-auto h-52 flex items-center flex-col text-center">
     <svg className="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
         <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
     </svg>
     <blockquote>
-        <p className="text-2xl italic font-medium text-gray-900 dark:text-white">
+        <p className="text-base italic font-medium text-gray-900 dark:text-white">
           {currentQuote.quote}
           </p>
     </blockquote>
@@ -44,7 +44,7 @@ const Home = () => {
     </figcaption>
 </figure>
 
-      <div className="border-red-100 border w-4/5 flex justify-center items-center h-40 rounded-md border-dashed">
+      <div className="border-red-100 border w-4/5 flex justify-center items-center h-32 rounded-md border-dashed">
         <Button type='create' onClick={onClickEm}/>
       </div>
       <TodoList/>
